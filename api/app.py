153b9c8c87aa302c375180ad06bf5e8fdf074080
atulit gaur <atulit23@gmail.com>
2023-12-16 19:25:03 +0530

    initial commit

---
+++ api/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify, request
+import joblib
+import spacy
+from spacy.lang.en import STOP_WORDS
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+app = Flask(__name__)
+
+nlp = spacy.load("en_core_web_sm")
+vectorizer = TfidfVectorizer()
+
+data = joblib.load('job_data.joblib')
+job_vectors = vectorizer.fit_transform(data)
+
+def preprocess_text(text):
+    doc = nlp(text)
+    print(doc)
+    tokens = [token.lemma_.lower() for token in doc if token not in STOP_WORDS]
+    return " ".join(tokens)
+
+def preprocess_text(text):
+    doc = nlp(text)
+    print(doc)
+    tokens = [token.lemma_.lower() for token in doc if token not in STOP_WORDS]
+    return " ".join(tokens)
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    data_to_return = []
+
+    input_data = request.json
+    input_ = input_data.get("query")
+
+    user_input = preprocess_text(input_)
+    candidate_vectors_new = vectorizer.transform([user_input])
+
+    similarity_scores_new = cosine_similarity(job_vectors, candidate_vectors_new)
+
+    dist_final = sorted(list(enumerate(similarity_scores_new)), reverse=True, key = lambda x : x[1])
+
+    for i in range(len(data)):
+        for j in dist_final[0:5]:
+            if(i == j[0]):
+                data_to_return.append({
+                    'title': data['Job Title'][i],
+                    'description': data['Job Description'][i],
+                    'salary': data['Salary Range'][i],
+                    'location': data['location'][i],
+                    'Country': data['Country'][i],
+                    'type': data['Work Type'][i],
+                    'role': data['Role'][i],
+                    'benefits': data['Benefits'][i],
+                    'skills': data['skills'][i],
+                    'qualifications': data['Qualifications'][i],
+                    'expirence': data['Experience'][i],
+                })
+    return jsonify(data_to_return)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
